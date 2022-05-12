d445d9db444cb75827262a3e894b08032a84b79d
Alina-Vardanovna <alinavardanovna@gmail.com>
2022-05-12 19:54:26 +0400

    Figure Example

--- src/homework/javabook/chapter3/FigureExample.java
+++ src/homework/javabook/chapter3/FigureExample.java
@@ -2,16 +2,51 @@ package homework.javabook.chapter3;
 
 public class FigureExample {
 
-    /*
-     *-ով սարքել այս տեսքի եռանկյուն
-     *
-     * *
-     * * *
-     * * * *
-     * * * * *
-     */
-
     public static void main(String[] args) {
+
+        /*
+         *-ով սարքել այս տեսքի եռանկյուն
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         */
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        /*
+         *-ով սարքել այս տեսքի եռանկյուն
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+         */
+
+        for (int i = 4; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+        /*
+         *-ով սարքել այս տեսքի եռանկյուն
+                 *
+               * *
+             * * *
+           * * * *
+         * * * * *
+         */
 
         for (int i = 0; i < 5; i++) {
 
