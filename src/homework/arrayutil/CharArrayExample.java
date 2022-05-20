2afea7a1bfe0238d0d7157aee0ca602e38c65fff
Alina-Vardanovna <alinavardanovna@gmail.com>
2022-05-20 21:58:49 +0400

    CharArrayExample

--- src/homework/arrayutil/CharArrayExample.java
+++ src/homework/arrayutil/CharArrayExample.java
@@ -29,37 +29,38 @@ public class CharArrayExample {
         System.out.println();
         char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
 
-        boolean IsLy = false;
+        boolean isLy = false;
         char optL = chars3[chars3.length - 2];
         char optY = chars3[chars3.length - 1];
         if (optL == 'l' && optY == 'y') {
-            IsLy = true;
+            isLy = true;
         }
-        System.out.println(IsLy);
+        System.out.println(isLy);
 
         //-----------------------------------------------------------------------------------------------
 
         System.out.println();
         char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a'};
-        boolean IsBob = false;
+        boolean isBob = false;
         char x = 'b';
         for (int i = 0; i < bobArray.length; i++) {
             if (bobArray[i] == x && bobArray[i] + 2 == x) ;
-            IsBob = true;
+            isBob = true;
         }
-        System.out.println(IsBob);
+        System.out.println(isBob);
 
         //-----------------------------------------------------------------------------------------------
 
         System.out.println();
-        char[] bobArray2 = {'b', 'a', 'b', 'o', 'b', 'a'};
-        boolean IsBob2 = false;
+        char[] bobArray2 = {'b', 'a', 'զ', 'o', 'b', 'a'};
+        boolean isBob2 = false;
         char y = 'b';
-        for (int i = 1; i < bobArray.length; i++) {
-            if (bobArray[i] == y && bobArray[i] + 2 == y) ;
-            IsBob2 = true;
+        for (int i = 0; i < bobArray2.length - 2; i++) {
+            if (bobArray2[i] == y && bobArray2[i + 2] == y) {
+                isBob2 = true;
+            }
         }
-        System.out.println(IsBob2);
+        System.out.println(isBob2);
 
 
         //-----------------------------------------------------------------------------------------------
@@ -72,6 +73,5 @@ public class CharArrayExample {
             }
         }
         System.out.println();
-
     }
 }
